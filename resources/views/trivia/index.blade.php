558db6b627bee1aaace941c1a5f5f9e772cff5c7
fujiiree120 <fujiiree120@gmail.com>
2020-02-02 23:15:31 +0900

    トップページ項目の追加#2

--- resources/views/trivia/index.blade.php
+++ resources/views/trivia/index.blade.php
@@ -4,42 +4,52 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
+        <div class="row index-body">
             <nav class="col-md-3">
-                <p>検索条件など</p>
+                <div class="card">
+                    <div class="card-header card-title">
+                        検索結果
+                    </div>
+                    <div class="card-body">
+                        <p>検索結果</p>
+                    </div>
+                </div>
             </nav>
             <article class="col-md-6">
-                <p>メインの豆知識</p>
-
-                        @forelse($trivias as $trivia)
-                        <div classs="row">
-                            <div class="card trivia-card">
-                                <div class="card-body">
-                                    <a href="{{ url('/') }}">{{ $trivia->name }}</a>
-                                    <p>Made by {{ $trivia->user->name }}</p>
-                                    <div class="trivia-vote">
-                                        <form method="get" action=""  class="text-right" name="myform" id = "my_form">
-                                            {{ csrf_field() }}
-                                            <input type="submit" value="へー:" class="vote-button">
-                                            <span class="vote-up">{{$trivia->vote_up }}</span>
-                                            <!-- <script type="text/javascript" src="/js/index.js"></script> -->
-                                        </form>
-                                        <form method="get" action=""  class="text-right" name="myform" id = "my_form">
-                                            {{ csrf_field() }}
-                                            <input type="submit" value="ちがうよ:" class="vote-button">
-                                            <span class="vote-down">{{$trivia->vote_down }}</span>
-                                            <!-- <script type="text/javascript" src="/js/index.js"></script> -->
-                                        </form>
-                                    </div>
+                @forelse($trivias as $trivia)
+                    <div classs="row">
+                        <div class="card trivia-card">
+                            <div class="card-body ">
+                                <a href="{{ url('/') }}" class="card-body-name">{{ $trivia->name }}</a>
+                                <p>Made by <a href="">{{ $trivia->user->name }}</a></p>
+                                <div class="trivia-vote">
+                                    <form method="get" action=""  class="text-right" name="myform" id = "my_form">
+                                        {{ csrf_field() }}
+                                        <input type="submit" value="へー:" class="vote-button">
+                                        <span class="vote-up">{{$trivia->vote_up }}</span>
+                                    </form>
+                                    <form method="get" action=""  class="text-right" name="myform" id = "my_form">
+                                        {{ csrf_field() }}
+                                        <input type="submit" value="ちがうよ:" class="vote-button">
+                                        <span class="vote-down">{{$trivia->vote_down }}</span>
+                                    </form>
                                 </div>
                             </div>
                         </div>
-                        @empty
-                            <p>豆知識はありません。</p>
-                        @endforelse
+                    </div>
+                @empty
+                    <p>豆知識はありません。</p>
+                @endforelse
             </article>
             <aside class="col-md-3">
-                <p>ユーザーランキング</p>
+                <div class="card">
+                    <div class="card-header card-title">
+                        ユーザーランキング
+                    </div>
+                    <div class="card-body">
+                        <p>ユーザーランキング</p>
+                    </div>
+                </div>
             </aside>
         </div>
     </div>
