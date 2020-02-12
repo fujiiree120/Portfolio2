098ac6fe5257d25aa5c629aa269f863ccd13ffd4
fujiiree120 <fujiiree120@gmail.com>
2020-02-12 22:39:51 +0900

    文言変更

--- resources/views/trivia/index.blade.php
+++ resources/views/trivia/index.blade.php
@@ -6,7 +6,7 @@
     <div class="container">
     <h1>{{ $title }}</h1>
     <p>このサイトはあなたが知っている雑学を共有し、皆で評価しあうSNSサービスです</p>
-    <p>左上のユーザー管理画面から投稿することができます</p>
+    <p>ヘッダーのユーザー管理画面から投稿することができます</p>
         <div class="row index-body">
             <div class="col-md-3 sticky order-md-2">
                 <aside>
